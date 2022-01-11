ceca129834ade81a106596d54ed93e9b106fe62e
yannickLega <yannick.lega@yahoo.com>
2022-01-11 13:26:55 +0100

    refactoring imports on the Header

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,20 +1,23 @@
 import React, { useState, useEffect } from "react";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import useScrollTrigger from "@material-ui/core/useScrollTrigger";
-import { makeStyles } from "@material-ui/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { useTheme } from "@material-ui/core/styles";
-import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
 
+import {
+  AppBar,
+  Toolbar,
+  useScrollTrigger,
+  Tabs,
+  Tab,
+  Button,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+  SwipeableDrawer,
+  IconButton,
+} from "@material-ui/core";
+
+import { makeStyles, useTheme } from "@material-ui/styles";
+
+import MenuIcon from "@material-ui/icons/Menu";
 
 import logo from "../../assets/logo.svg";
 
@@ -230,7 +233,6 @@ export default function Header(props) {
         classes={{ paper: classes.drawer }}
       >
         <div className={classes.toolbarMargin} />
-
       </SwipeableDrawer>
       <IconButton
         className={classes.drawerIconContainer}
