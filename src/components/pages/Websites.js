d7537bb0b3109f807959db180ccfad3a02b2d489
yannickLega <yannick.lega@yahoo.com>
2022-01-19 16:09:03 +0100

    add website page => services section done

--- src/components/pages/Websites.js
+++ src/components/pages/Websites.js
@@ -1,51 +1,293 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 300,
-  },
-  media: {
-    height: 200,
-  },
-});
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
-export default function Home() {
+import {
+  Typography,
+  Grid,
+  useMediaQuery,
+  Hidden,
+  IconButton,
+} from "@material-ui/core";
+
+import backArrow from "../../assets/backArrow.svg";
+import forwardArrow from "../../assets/forwardArrow.svg";
+import analytics from "../../assets/analytics.svg";
+import seo from "../../assets/seo.svg";
+import outreach from "../../assets/outreach.svg";
+import ecommerce from "../../assets/ecommerce.svg";
+import CallToAction from "../ui/CallToAction";
+
+const useStyles = makeStyles((theme) => ({
+  rowContainer: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+    },
+  },
+  heading: {
+    maxWidth: "40em",
+  },
+  arrowContainer: {
+    marginTop: "0.5em",
+  },
+  paragraphContainer: {
+    maxWidth: "30em",
+  },
+}));
+
+export default function Websites(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
+  const matchesXS = useMediaQuery(theme.breakpoints.down("xs"));
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://images.unsplash.com/photo-1586769203114-cf7f0f37297a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGxpemFyZHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+    <Grid container direction="column">
+      <Grid
+        item
+        container
+        justify={matchesMD ? "center" : undefined}
+        className={classes.rowContainer}
+        style={{ marginTop: matchesXS ? "1em" : "2em" }}
+      >
+        <Hidden mdDown>
+          <Grid
+            item
+            className={classes.arrowContainer}
+            style={{ marginRight: "1em", marginLeft: "-3em" }}
+          >
+            <IconButton
+              style={{ backgroundColor: "transparent" }}
+              component={Link}
+              to="/mobileapps"
+              onClick={() => props.setSelectedIndex(2)}
+              disableRipple
+            >
+              <img src={backArrow} alt="back to mobileapps page" />
+            </IconButton>
+          </Grid>
+        </Hidden>
+        <Grid item container direction="column" className={classes.heading}>
+          <Grid item>
+            <Typography align={matchesMD ? "center" : undefined} variant="h2">
+              Website Development
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              align={matchesMD ? "center" : undefined}
+              variant="body1"
+              paragraph
+            >
+              Having a website is a necessity in today’s business world. They
+              give you one central, public location to let people know who you
+              are, what you do, and why you’re the best at it.
+            </Typography>
+            <Typography
+              align={matchesMD ? "center" : undefined}
+              variant="body1"
+              paragraph
+            >
+              From simply having your hours posted to having a full fledged
+              online store, making yourself as accessible as possible to users
+              online drives growth and enables you to reach new customers.
+            </Typography>
+          </Grid>
+        </Grid>
+        <Hidden mdDown>
+          <Grid
+            item
+            className={classes.arrowContainer}
+            style={{ marginRight: "-3em", marginLeft: "1em" }}
+          >
+            <IconButton
+              style={{ backgroundColor: "transparent" }}
+              component={Link}
+              to="/services"
+              onClick={() => props.setSelectedIndex(0)}
+              disableRipple
+            >
+              <img src={forwardArrow} alt="forward to services page" />
+            </IconButton>
+          </Grid>
+        </Hidden>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesSM ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems="center"
+        style={{ marginTop: "15em" }}
+      >
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography
+                variant="h4"
+                gutterBottom
+                align={matchesSM ? "center" : undefined}
+              >
+                Analytics
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={analytics}
+                alt="graph with magnifying glass revealing 1's and 0's"
+                style={{ marginLeft: "-2.75em" }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item className={classes.paragraphContainer}>
+          <Typography variant="body1" align={matchesSM ? "center" : undefined}>
+            Knowledge is power, and data is 21st Century gold. Analyzing this
+            data can reveal hidden patterns and trends in your business,
+            empowering you to make smarter decisions with measurable effects.
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesSM ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems="center"
+        justify="flex-end"
+        style={{ marginTop: "15em", marginBottom: "15em" }}
+      >
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography variant="h4" gutterBottom align="center">
+                E-Commerce
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={ecommerce} alt="world outline made of dollar signs" />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          style={{ marginLeft: matchesSM ? 0 : "1em" }}
+          className={classes.paragraphContainer}
+        >
+          <Typography
+            align={matchesSM ? "center" : undefined}
+            variant="body1"
+            paragraph
+          >
+            It’s no secret that people like to shop online.
           </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+          <Typography
+            align={matchesSM ? "center" : undefined}
+            variant="body1"
+            paragraph
+          >
+            In 2017 over $2.3 trillion was spent in e-commerce, and it’s time
+            for your slice of that pie.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesSM ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems="center"
+      >
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography
+                variant="h4"
+                gutterBottom
+                align={matchesSM ? "center" : undefined}
+              >
+                Outreach
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img src={outreach} alt="megaphone" />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          style={{ marginLeft: matchesSM ? 0 : "1em" }}
+          className={classes.paragraphContainer}
+        >
+          <Typography align={matchesSM ? "center" : undefined} variant="body1">
+            Draw people in with a dazzling website. Showing off your products
+            online is a great way to help customers decide what’s right for them
+            before visiting in person.
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesSM ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems="center"
+        justify="flex-end"
+        style={{ marginTop: "15em", marginBottom: "15em" }}
+      >
+        <Grid item>
+          <Grid container direction="column">
+            <Grid item>
+              <Typography variant="h4" gutterBottom align="center">
+                Search Engine
+                <br />
+                Optimization
+              </Typography>
+            </Grid>
+            <Grid item>
+              <img
+                src={seo}
+                alt="website standing standing on winner's podium"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          style={{ marginLeft: matchesSM ? 0 : "1em" }}
+          className={classes.paragraphContainer}
+        >
+          <Typography
+            align={matchesSM ? "center" : undefined}
+            variant="body1"
+            paragraph
+          >
+            How often have you ever been to the second page of Google results?
+          </Typography>
+          <Typography
+            align={matchesSM ? "center" : undefined}
+            variant="body1"
+            paragraph
+          >
+            If you’re like us, probably never.
+          </Typography>
+          <Typography
+            align={matchesSM ? "center" : undefined}
+            variant="body1"
+            paragraph
+          >
+            Customers don’t go there either, so we make sure your website is
+          </Typography>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <CallToAction setValue={props.setValue} />
+      </Grid>
+    </Grid>
   );
 }
