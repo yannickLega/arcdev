c32b517a99a23cd65c4b28fe6899e55312dd0ccb
yannickLega <yannick.lega@yahoo.com>
2022-01-20 22:43:14 +0100

    add about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,51 +1,272 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 300,
-  },
-  media: {
-    height: 200,
-  },
-});
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 
-export default function About() {
+import {
+  Typography,
+  Grid,
+  useMediaQuery,
+  Avatar,
+  Hidden,
+} from "@material-ui/core";
+
+import history from "../../assets/history.svg";
+import profile from "../../assets/founder.jpg";
+import yearbook from "../../assets/yearbook.svg";
+import puppy from "../../assets/puppy.svg";
+
+import CallToAction from "../ui/CallToAction";
+
+const useStyles = makeStyles((theme) => ({
+  rowContainer: {
+    paddingLeft: "5em",
+    paddingRight: "5em",
+    [theme.breakpoints.down("sm")]: {
+      paddingLeft: "1.5em",
+      paddingRight: "1.5em",
+    },
+  },
+  missionStatement: {
+    fontStyle: "italic",
+    fontWeight: 300,
+    fontSize: "1.5rem",
+    maxWidth: "50em",
+    lineHeight: 1.4,
+  },
+  avatar: {
+    width: "20em",
+    height: "20em",
+    [theme.breakpoints.down("sm")]: {
+      width: "15em",
+      height: "15em",
+      maxWidth: 300,
+      maxHeight: 300,
+    },
+  },
+}));
+
+export default function About(props) {
   const classes = useStyles();
+  const theme = useTheme();
+  const matchesMD = useMediaQuery(theme.breakpoints.down("md"));
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://media.istockphoto.com/photos/yellow-blue-lizard-panther-chameleon-isolated-on-white-background-picture-id1094540956?b=1&k=20&m=1094540956&s=170667a&w=0&h=8HemWtGQ8okpFYt0hRg9muNMnrxubMxfcQBiZ6vOH4c="
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Lizard
+    <Grid container direction="column">
+      <Grid
+        item
+        className={classes.rowContainer}
+        style={{ marginTop: matchesMD ? "1em" : "2em" }}
+      >
+        <Typography variant="h2" align={matchesMD ? "center" : undefined}>
+          About Us
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        justify="center"
+        className={classes.rowContainer}
+        style={{ marginTop: "3em" }}
+      >
+        <Typography
+          variant="h4"
+          align="center"
+          className={classes.missionStatement}
+        >
+          Whether it be person to person, business to consumer, or an individual
+          to their interests, technology is meant to bring us closer to what we
+          care about in the best way possible. Arc Development will use that
+          principle to provide fast, modern, inexpensive, and aesthetic software
+          to the Midwest and beyond.
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        direction={matchesMD ? "column" : "row"}
+        className={classes.rowContainer}
+        alignItems={matchesMD ? "center" : undefined}
+        justify="space-around"
+        style={{ marginTop: "10em", marginBottom: "10em" }}
+      >
+        <Grid item>
+          <Grid
+            item
+            container
+            direction="column"
+            lg
+            style={{ maxWidth: "35em" }}
+          >
+            <Grid item>
+              <Typography
+                align={matchesMD ? "center" : undefined}
+                variant="h4"
+                gutterBottom
+              >
+                History
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography
+                variant="body1"
+                paragraph
+                style={{ fontWeight: 700, fontStyle: "italic" }}
+                align={matchesMD ? "center" : undefined}
+              >
+                We’re the new kid on the block.
+              </Typography>
+              <Typography
+                variant="body1"
+                paragraph
+                align={matchesMD ? "center" : undefined}
+              >
+                Founded in 2019, we’re ready to get our hands on the world’s
+                business problems.
+              </Typography>
+              <Typography
+                variant="body1"
+                paragraph
+                align={matchesMD ? "center" : undefined}
+              >
+                It all started with one question: Why aren’t all businesses
+                using available technology? There are many different answers to
+                that question: economic barriers, social barriers, educational
+                barriers, and sometimes institutional barriers.
+              </Typography>
+              <Typography
+                variant="body1"
+                paragraph
+                align={matchesMD ? "center" : undefined}
+              >
+                We aim to be a powerful force in overcoming these obstacles.
+                Recent developments in software engineering and computing power,
+                compounded by the proliferation of smart phones, has opened up
+                infinite worlds of possibility. Things that have always been
+                done by hand can now be done digitally and automatically, and
+                completely new methods of interaction are created daily. Taking
+                full advantage of these advancements is the name of the game.
+              </Typography>
+              <Typography
+                variant="body1"
+                paragraph
+                align={matchesMD ? "center" : undefined}
+              >
+                All this change can be a lot to keep up with, and that's where
+                we come in.
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          <Grid item container justify="center" lg>
+            <img
+              src={history}
+              alt="quill pen sitting on top of book"
+              style={{ maxHeight: matchesMD ? 200 : "22em" }}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid
+        item
+        container
+        direction="column"
+        alignItems="center"
+        className={classes.rowContainer}
+        style={{ marginBottom: "15em" }}
+      >
+        <Grid item>
+          <Typography variant="h4" gutterBottom align="center">
+            Team
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+        </Grid>
+        <Grid item>
+          <Typography variant="body1" paragraph align="center">
+            Zachary Reece, Founder
           </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
+          <Typography variant="body1" paragraph align="center">
+            I started coding when I was 9 years old.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Avatar src={profile} alt="founder" className={classes.avatar} />
+        </Grid>
+        <Grid item container justify={matchesMD ? "center" : undefined}>
+          <Hidden lgUp>
+            <Grid item lg style={{ maxWidth: "45em", padding: "1.25em" }}>
+              <Typography variant="body1" align="center" paragraph>
+                I taught myself basic coding from a library book in third grade,
+                and ever since then my passion has solely been set on learning —
+                learning about computers, learning mathematics and philosophy,
+                studying design, always just learning.
+              </Typography>
+              <Typography variant="body1" align="center" paragraph>
+                Now I’m ready to apply everything I’ve learned, and to help
+                others with the intuition i have developed.
+              </Typography>
+            </Grid>
+          </Hidden>
+          <Grid
+            item
+            container
+            direction="column"
+            lg
+            alignItems={matchesMD ? "center" : undefined}
+            style={{ marginBottom: matchesMD ? "2.5em" : 0 }}
+          >
+            <Grid item>
+              <img
+                src={yearbook}
+                alt="yearbook page about founder"
+                style={{ maxWidth: matchesMD ? 300 : undefined }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="caption">
+                a page from my Sophomore yearbook
+              </Typography>
+            </Grid>
+          </Grid>
+          <Hidden mdDown>
+            <Grid item lg style={{ maxWidth: "45em", padding: "1.25em" }}>
+              <Typography variant="body1" align="center" paragraph>
+                I taught myself basic coding from a library book in third grade,
+                and ever since then my passion has solely been set on learning —
+                learning about computers, learning mathematics and philosophy,
+                studying design, always just learning.
+              </Typography>
+              <Typography variant="body1" align="center" paragraph>
+                Now I’m ready to apply everything I’ve learned, and to help
+                others with the intuition i have developed.
+              </Typography>
+            </Grid>
+          </Hidden>
+          <Grid
+            item
+            container
+            direction="column"
+            lg
+            alignItems={matchesMD ? "center" : "flex-end"}
+          >
+            <Grid item>
+              <img
+                src={puppy}
+                alt="grey spotted puppy"
+                style={{ maxWidth: matchesMD ? 300 : undefined }}
+              />
+            </Grid>
+            <Grid item>
+              <Typography variant="caption">
+                my miniature dapple dachshund, Sterling
+              </Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        <CallToAction setValue={props.setValue}/>
+      </Grid>
+    </Grid>
   );
 }
